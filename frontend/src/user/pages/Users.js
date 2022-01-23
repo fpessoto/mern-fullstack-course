67fce9ad7a984fbc898f61ca8211bee348699194
Felipe Pessoto Medeiros Guimarães <felipe.pessoto@hotmail.com>
2022-01-23 16:22:20 -0300

    feat: added user places dummy list

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import UsersList from "../components/UsersList";
+import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-      id: "u1",
-      name: "Felipe Pessoto",
+      id: 'u1',
+      name: 'Max Schwarz',
       image:
-        "https://www.farmersalmanac.com/wp-content/uploads/2020/11/words-for-snow-AdobeStock_301401130.jpg",
-      places: 3,
-    },
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
   ];
 
   return <UsersList items={USERS} />;
